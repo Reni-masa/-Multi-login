f7dc3735616a502c18ce8c5433488d5bb72aaca5
SugitaMasataka <mstk.ambit@gmail.com>
2022-02-13 19:39:43 +0900

    add:サーバー側で画像をリサイズ

--- laravel/app/Http/Controllers/Owner/ShopController.php
+++ laravel/app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -19,14 +20,24 @@ class ShopController extends Controller
 
     public function update(Request $request, $id)
     {
-
-
+        // リクエストファイル取得
         $imageFile = $request->filename;
 
         if( !is_null($imageFile)) {
-            $path = Storage::disk("public")->putFile('shops', $imageFile);
-            $filePath = '/storage/' . $path;
 
+            // ランダムなファイル名を生成
+            $uniqueCode = uniqid(rand());
+            // 拡張子取得
+            $extension = $imageFile->extension();
+            // 保存するファイル名
+            $uniquFileName = $uniqueCode.'.'.$extension;
+            // リサイズ
+            $resizeImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            // サーバー側に画像保存
+            Storage::disk("public")->put('shops/'.$uniquFileName, $resizeImage);
+
+            // 画像パスを保存
+            $filePath = '/storage/shops/' . $uniquFileName;
             $shop = Auth::user()->shop;
             $shop->filename = $filePath;
             $shop->save();
