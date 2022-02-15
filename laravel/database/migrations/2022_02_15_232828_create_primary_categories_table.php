af7d11cf83fb80e81ecdbe30966fc3487ad3c2b6
SugitaMasataka <mstk.ambit@gmail.com>
2022-02-15 23:42:42 +0900

    add：カテゴリーテーブル作成

---
+++ laravel/database/migrations/2022_02_15_232828_create_primary_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrimaryCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('primary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('カテゴリー名');
+            $table->integer('sort_order')->comment('並び順');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('primary_categories');
+    }
+}
---
+++ laravel/database/migrations/2022_02_15_232947_create_secondary_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSecondaryCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secondary_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('カテゴリー名');
+            $table->integer('sort_order')->comment('並び順');
+            $table->foreignId('primary_categories_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secondary_categories');
+    }
+}
